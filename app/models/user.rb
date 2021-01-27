12e923dc4b85bfa10f6e54840b6ead4934a22275
Allie <alliecaton3@gmail.com>
2021-01-27 15:30:35 -0500

    commit tree 98be60977b6371ebd30847e3b0b14b9687c4ec39

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
+class User < ActiveRecord::Base
 
+# attr_reader :username, :password, :balance
 
+#     def initialize(username:, password:, balance:)
+#         @username = username 
+#         @password = password
+#         @balance = balance
+#     end
+
+end 
